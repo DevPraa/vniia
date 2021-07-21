e6b1d878633a0928eeb192cbbd7b054ee0a728d8
Viktor Tolkachev <viktor.tolkachev@galaktika.ru>
2021-07-21 04:18:17 +0300

    Добавлены переопределения методов RepositoryBase для DocumentPositionRepository, чтобы они подгружали связный документ при запросе позиции документа.
    В DocumentPositionRepository добавлен новый метод который загружает все позиции документа связанные с  переданным идентификатором документа.
    В DocumentPositionController добавлено новый метод GetDocumentRelatedDocumentPositions возвращает все позиции документа связанные с  переданным идентификатором документа.
    В DocumentPositionController исправлена ошибка из-за который не срабатывали маршруты к данному API .

--- VNIIA/VNIIA.Server/Controllers/DocumentPositionController.cs
+++ VNIIA/VNIIA.Server/Controllers/DocumentPositionController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 using VNIIA.Server.Models;
@@ -9,14 +10,23 @@ using VNIIA.Server.Repository;
 
 namespace VNIIA.Server.Controllers
 {
+	[ApiController]
+	[Route("[controller]/[action]")]
 	public class DocumentPositionController : BaseDbController<DocumentPosition, DocumentPositionRepository>
 	{
 		private readonly ILogger<DocumentPositionController> _logger;
+		private readonly DocumentPositionRepository _repository;
 
 		public DocumentPositionController(DocumentPositionRepository repository, ILogger<DocumentPositionController> logger) : base(repository)
 		{
 			_logger = logger;
+			_repository = repository;
+		}
 
+		[HttpGet("{id}")]
+		public ActionResult<IEnumerable<DocumentPosition>> GetDocumentRelatedDocumentPositions(int id)
+		{
+			return new ObjectResult(_repository.GetDocumentRelatedDocumentPositions(id));
 		}
 	}
 }
--- VNIIA/VNIIA.Server/Repository/DocumentPositionRepository.cs
+++ VNIIA/VNIIA.Server/Repository/DocumentPositionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using Microsoft.EntityFrameworkCore;
@@ -11,12 +12,35 @@ namespace VNIIA.Server.Repository
 	/// <summary>
 	/// Репозиторий для работы с позициями документов
 	/// </summary>
-	public class DocumentPositionRepository : RepositoryBase<DocumentPosition, DatabaseContext>
+	public sealed class DocumentPositionRepository : RepositoryBase<DocumentPosition, DatabaseContext>
 	{
+		DatabaseContext _dbContext;
 		public DocumentPositionRepository(DatabaseContext dbContext ) : base(dbContext)
 		{
-
+			_dbContext = dbContext;
 		}
 
+		public override IEnumerable<DocumentPosition> Get()
+		{
+			return _dbContext.DocumentPostions.Include(prop => prop.Document).AsEnumerable();
+		}
+
+		public override IEnumerable<DocumentPosition> Get(Func<DocumentPosition, bool> predicate)
+		{
+			return _dbContext.DocumentPostions.Include(prop => prop.Document).AsQueryable().Where(predicate).AsEnumerable();
+		}
+
+		public override DocumentPosition FindById(int id)
+		{
+			return _dbContext.DocumentPostions.Include(prop => prop.Document).FirstOrDefault(c => c.Number == id);
+		}
+
+		/// <summary>
+		/// Получить позиции документа, связанные с документом
+		/// </summary>
+		public IEnumerable<DocumentPosition> GetDocumentRelatedDocumentPositions(int document)
+		{
+			return Get(c=>c.DocumentId == document);
+		}
 	}
 }
